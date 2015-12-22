fdbfa01741459586d3d1dbb8dc5ff4556ea3b362
Jacob Blakely <jacob@codekirei.com>
2015-12-21 22:26:45 -0800

    start on tests

--- test/index.js
+++ test/index.js
@@ -2,4 +2,23 @@
 
 // modules
 //----------------------------------------------------------
-const Hari = require('../lib/hari')
+import Hari from '../'
+import test from 'ava'
+import sinon from 'sinon'
+
+// setup
+//----------------------------------------------------------
+const hari = new Hari()
+const clock = sinon.useFakeTimers()
+
+// tests
+//----------------------------------------------------------
+test('constructor', t => {
+  hari.init()
+  t.is(hari.running, false, 'running')
+  t.is(hari.runs, 0, 'runs')
+  t.is(hari.start, 0, 'start')
+  t.is(hari.now, void 0, 'now')
+  t.is(hari.command, void 0, 'command')
+  t.is(hari.args, void 0, 'args')
+})
