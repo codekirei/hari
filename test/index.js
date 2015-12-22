3d76a6c0b1f06372a6b4c25fc6d30e3c5f08123e
Jacob Blakely <jacob@codekirei.com>
2015-12-21 23:47:47 -0800

    write some more tests

--- test/index.js
+++ test/index.js
@@ -1,18 +1,39 @@
 'use strict'
 
+//----------------------------------------------------------
 // modules
 //----------------------------------------------------------
-import Hari from '../'
-import test from 'ava'
-import sinon from 'sinon'
+// node
 import {EventEmitter} from 'events'
 import childProcess from 'child_process'
 
+// npm
+import sinon from 'sinon'
+import test from 'ava'
+
+// local
+import Hari from '../'
+
+//----------------------------------------------------------
+// hooks
+//----------------------------------------------------------
+let hari
+let clock
+
+test.beforeEach(t => {
+  hari = new Hari()
+  clock = sinon.useFakeTimers()
+})
+
+test.afterEach(t => {
+  hari = void 0
+  clock.restore()
+})
+
+//----------------------------------------------------------
 // tests
 //----------------------------------------------------------
 test('constructor', t => {
-  const hari = new Hari()
-  const clock = sinon.useFakeTimers()
   hari.init()
   t.is(hari.running, false, 'running')
   t.is(hari.runs, 0, 'runs')
@@ -26,16 +47,12 @@ test.skip('clear', t => {
 })
 
 test('convertHours', t => {
-  const hari = new Hari()
   t.is(hari.convertHours(12), 0)
   t.is(hari.convertHours(13), 1)
   t.is(hari.convertHours(7), 7)
 })
 
 test('debounce', t => {
-  // setup
-  const clock = sinon.useFakeTimers()
-  const hari = new Hari()
   const clear = sinon.stub(hari, 'clear', () => new EventEmitter())
   const run = sinon.stub(hari, 'run')
   hari.command = true
@@ -44,4 +61,57 @@ test('debounce', t => {
   t.is(hari.runs, 1, 'runs')
   clock.tick(50)
   t.false(hari.running, 'stopped running')
+})
+
+test('duration', t => {
+  const stub = sinon.stub(hari, 'parseMs')
+  hari.start = Date.parse(new Date())
+  hari.now = new Date()
+  hari.now.setSeconds(3)
+  hari.duration()
+  t.true(stub.called, 'call parseMs')
+  t.true(stub.calledWith(3000), 'call parseMs with correct value')
+})
+
+test.skip('header', t => {
+})
+
+test.skip('init', t => {
+})
+
+test('longestStr', t =>
+  t.is(hari.longestStr(['one', 'two', 'three', 'four']), 5)
+)
+
+test('padStrs', t =>
+  t.same(
+    hari.padStrs(['a', 'b ', 'c  '], 3)
+    , ['a  ', 'b  ', 'c  ']
+  )
+)
+
+test('parseCommand', t => {
+  hari.parseCommand('npm test -v')
+  t.is(hari.command, 'npm', 'command')
+  t.same(hari.args, ['test', '-v'], 'args')
+})
+
+test.skip('parseMs', t => {
+})
+
+test('prepTime', t => {
+  t.is(hari.prepTime(12), '12', 'stringified')
+  t.is(hari.prepTime(3), '03', 'prepended')
+})
+
+test.skip('readPkg', t => {
+})
+
+test.skip('run', t => {
+})
+
+test.skip('time', t => {
+})
+
+test.skip('watch', t => {
 })
