5f4c26699c2a8b320d7d76ec91763637e09e67eb
Jacob Blakely <jacob@codekirei.com>
2015-12-23 16:45:29 -0800

    split out tests for pure methods

--- test/index.js
+++ test/index.js
@@ -39,13 +39,6 @@ test('clear', t => {
   proc.spawn.restore()
 })
 
-test('convertHours', t => {
-  const hari = new Hari()
-  t.is(hari.convertHours(12), 0)
-  t.is(hari.convertHours(13), 1)
-  t.is(hari.convertHours(7), 7)
-})
-
 test('debounce', t => {
   const hari = new Hari()
   const clock = sinon.useFakeTimers()
@@ -140,19 +133,6 @@ test('init', async t => {
   clock.restore()
 })
 
-test('longestStr', t => {
-  const hari = new Hari()
-  t.is(hari.longestStr(['one', 'two', 'three', 'four']), 5)
-})
-
-test('padStrs', t => {
-  const hari = new Hari()
-  t.same(
-    hari.padStrs(['a', 'b ', 'c  '], 3)
-    , ['a  ', 'b  ', 'c  ']
-  )
-})
-
 test('parseCommand', t => {
   const hari = new Hari()
   hari.parseCommand('./test')
@@ -160,20 +140,6 @@ test('parseCommand', t => {
   hari.parseCommand('npm test -v')
   t.is(hari.command, 'npm')
   t.same(hari.args, ['test', '-v'])
-})
-
-test('parseMs', t => {
-  const hari = new Hari()
-  t.is(hari.parseMs(1000), '0:00:01')
-  t.is(hari.parseMs(62500), '0:01:02')
-  t.is(hari.parseMs(3723000), '1:02:03')
-  t.is(hari.parseMs(36123000), '10:02:03')
-})
-
-test('prepTime', t => {
-  const hari = new Hari()
-  t.is(hari.prepTime(12), '12', 'stringified')
-  t.is(hari.prepTime(3), '03', 'prepended')
 })
 
 test('readPkg', async t => {
