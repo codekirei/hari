1e2dc401ba67a8e2b0929c0ac9bd862f3d889bf3
Jacob Blakely <jacob@codekirei.com>
2016-01-02 12:50:29 -0800

    back to ava for tests :sweat:

--- test/index.js
+++ test/index.js
@@ -4,61 +4,23 @@
 // modules
 //----------------------------------------------------------
 // node
-const process = require('child_process')
-const Emitter = require('events').EventEmitter
+import test from 'ava'
 
 // npm
-const t = require('chai').assert
-const sinon = require('sinon')
-const chokidar = require('chokidar')
+// const sinon = require('sinon')
+// const chokidar = require('chokidar')
 
 // local
-const Hari = require('../')
-const util = require('../lib/utils')
+import Hari from '../'
 
 //----------------------------------------------------------
 // tests
 //----------------------------------------------------------
-describe('hari', () => {
-  beforeEach(() => {
-    sinon.stub(process, 'spawn')
-    sinon.stub(chokidar, 'watch').returns(new Emitter())
-  })
-
-  it('autoruns', cb => {
-    sinon.spy(util, 'readJson')
-    const hari = new Hari()
-    t.isTrue(util.readJson.calledOnce)
-    return cb()
-  })
-
-  afterEach(() => {
-    process.spawn.restore()
-    chokidar.watch.restore()
-  })
+test('constructor', t => {
+  const hari = new Hari()
+  t.false(hari.running)
+  t.is(hari.runs, 0)
+  t.is(hari.cmd, void 0)
+  t.is(hari.startTime, void 0)
+  t.is(hari.timestamp, void 0)
 })
-
-// test('hari - default path', t => {
-//   sinon.spy(util, 'readJson')
-//   sinon.stub(console, 'log')
-
-//   hari()
-//   t.true(util.readJson.calledOnce)
-//   t.true(util.readJson.calledWith('./package.json'))
-
-//   util.readJson.restore()
-//   console.log.restore()
-// })
-
-// test('hari - custom path', t => {
-//   sinon.spy(util, 'readJson')
-
-//   hari('fixture')
-//   t.true(util.readJson.calledOnce)
-//   t.same(
-//     util.readJson.firstCall.returnValue._fulfillmentHandler0
-//     , hari.init
-//   )
-
-//   util.readJson.restore()
-// })
